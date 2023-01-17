fc1e338cce4a36fbc2f7576d5ea4b10435a25e9e
toye <anibiseun@gmail.com>
2023-01-17 13:35:19 +0100

    finish

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const previousOperation=document.getElementById('#user-input')
-const currentOperation=document.getElementById('#result')
-const clearBtn=document.getElementById('#clear')
-const plusminusBtn=document.getElementById('#plusminus')
+const previousOperation=document.getElementById('user-input')
+const currentOperation=document.getElementById('result')
+const clearBtn=document.getElementById('clear')
+const plusminusBtn=document.getElementById('plusminus')
 const operatorBtn=document.querySelectorAll('[data-operator]')
 const numberBtn=document.querySelectorAll('[data-number]')
-const equalsBtn=document.getElementById('#equals')
-const percentBtn=document.getElementById('#percent')
+const equalsBtn=document.getElementById('equals')
+const percentBtn=document.getElementById('percent')
+let op;
 
 function appendNumber(number){
     if (number === '.' && currentOperation.textContent.includes('.')) return;
@@ -74,6 +75,77 @@ clearBtn.addEventListener('click',clear)
 plusminusBtn.addEventListener('click',plusminus)
 percentBtn.addEventListener('click',percentage)
 equalsBtn.addEventListener('click',compute)
+// const previousOperation = document.getElementById('user-input');
+// const currentOperation = document.getElementById('result');
+// const clearButton = document.getElementById('clear');
+// const equalsButton = document.getElementById('equals');
+// const plusMinus = document.getElementById('plusminus');
+// const percent = document.getElementById('percent');
+// const numberButtons = document.querySelectorAll('[data-number]');
+// const operationButtons = document.querySelectorAll('[data-operator]');
+// let op;
+
+// function appendNumber(number) {
+// 	if (number === '.' && currentOperation.textContent.includes('.')) return;
+// 	currentOperation.textContent += number;
+// }
+// function chooseOperation(operation) {
+// 	if (currentOperation.textContent === '') return;
+// 	if (previousOperation.textContent !== '') {
+// 		compute();
+// 	}
+// 	op = operation;
+// 	previousOperation.textContent = `${currentOperation.textContent} ${op}`;
+// 	currentOperation.textContent = '';
+// }
+// function compute() {
+// 	let result;
+// 	const pv = parseFloat(previousOperation.textContent);
+// 	const cr = parseFloat(currentOperation.textContent);
+// 	if (isNaN(pv) && isNaN(cr)) return;
+// 	switch (op) {
+// 		case '+':
+// 			result = pv + cr;
+// 			break;
+// 		case '-':
+// 			result = pv - cr;
+// 			break;
+// 		case 'X':
+// 			result = pv * cr;
+// 			break;
+// 		case '/':
+// 			result = pv / cr;
+// 			break;
+// 		default:
+// 			return;
+// 	}
+// 	currentOperation.textContent = result;
+// 	previousOperation.textContent = '';
+// }
+// function clear() {
+// 	currentOperation.textContent = '';
+// 	previousOperation.textContent = '';
+// }
+// function minus() {
+// 	currentOperation.textContent = currentOperation.textContent * -1;
+// }
+// function percentage() {
+// 	currentOperation.textContent = currentOperation.textContent * 0.01;
+// }
+// numberButtons.forEach((button) => {
+// 	button.addEventListener('click', () => {
+// 		appendNumber(button.textContent);
+// 	});
+// });
+// operationButtons.forEach((button) => {
+// 	button.addEventListener('click', () => {
+// 		chooseOperation(button.textContent);
+// 	});
+// });
+// clearButton.addEventListener('click', clear);
+// equalsButton.addEventListener('click', compute);
+// plusMinus.addEventListener('click', minus);
+// percent.addEventListener('click', percentage);
 
 
 
